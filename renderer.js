ad80bbbde85c75bf491853e2ce3e6d8ee92e79a7
minakawa-daiki <minakawa.daiki.as@gmail.com>
2018-11-19 23:16:33 +0900

    サンプルビデオ再生実装

--- renderer.js
+++ renderer.js
@@ -3,8 +3,23 @@
 // All of the Node.js APIs are available in this process.
 
 const serialPort = require('serialport');
+const fs = require('fs');
+
 serialPort.list(function(err, ports) {
   ports.forEach(function(port){
     console.log(port);
   });
+});
+
+const videoList = document.getElementById("video-list");
+
+fs.readdir('./videos/', function(err, files){
+    if (err) throw err;
+    files.forEach(file => {
+        if(file !== ".keep") {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(file));
+            videoList.appendChild(li);
+        }
+    });
 });
